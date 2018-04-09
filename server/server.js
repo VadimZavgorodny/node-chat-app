70335df1e9d97c116186e982b770aa62e74b70e4
VadimZavgorodny <va.zavgorodny@gmail.com>
2018-04-09 19:52:00 +0300

    Connect and disconnect event

--- server/server.js
+++ server/server.js
@@ -1,11 +1,27 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIo = require('socket.io');
 const port = process.env.PORT || 3000;
 
 const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
 
 app.use(express.static('public'));
 
-app.listen(port, function () {
+io.on('connection', (socket)=>{
+    console.log('New user connected');
+    socket.on('disconnect', function() {
+        console.log('Disconnect');
+    });
+});
+
+
+io.on('disconnect', function () {
+    console.log('Got disconnect!');
+});
+
+server.listen(port, function () {
     console.log(`Example app listening on port ${port}!`);
 });
