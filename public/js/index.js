e242333397f22c7bd1982f0134307fb0efc49fa1
VadimZavgorodny <va.zavgorodny@gmail.com>
2018-04-14 18:28:57 +0300

    Added form for messages and show that

--- public/js/index.js
+++ public/js/index.js
@@ -10,4 +10,23 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function (message) {
     console.log('New Message', message);
+    let li = $('<li></li>');
+    li.text(`${message.from}: ${message.text}`);
+
+    $('#messages').append(li);
+});
+
+socket.emit('createMessage', {from: 'Frank', text: 'Hi'}, function(data) {
+    console.log(data);
+});
+
+$('#message-form').on('submit', function (e) {
+    e.preventDefault();
+
+    socket.emit('createMessage', {
+        from: 'User',
+        text: $('[name=message]').val()
+    }, function (data) {
+        console.log(data);
+    });
 });
