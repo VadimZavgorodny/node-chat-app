d3c56307acb15e6d48a5e8ac8771f42ca0658a05
VadimZavgorodny <va.zavgorodny@gmail.com>
2018-04-21 12:20:15 +0300

    Scroll to bottom if user close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,21 @@
 const socket = io();
 
+function scrollToBottom() {
+    //Selectors
+    let messages = $('#messages');
+    let newMessage = messages.children('li:last-child');
+    //Heights
+    let clientHeight = messages.prop('clientHeight');
+    let scrollTop = messages.prop('scrollTop');
+    let scrollHeight = messages.prop('scrollHeight');
+    let newMessageHeight = newMessage.innerHeight();
+    let lastMessageHeight = newMessage.prev().innerHeight();
+
+    if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+        messages.scrollTop(scrollHeight);
+    }
+}
+
 socket.on('connect', function () {
     console.log('Connected to server');
 });
@@ -19,6 +35,7 @@ socket.on('newLocationMessage', function (message) {
     });
 
     $('#messages').append(html);
+    scrollToBottom();
 });
 
 socket.on('newMessage', function (message) {
@@ -31,6 +48,7 @@ socket.on('newMessage', function (message) {
     });
 
     $('#messages').append(html);
+    scrollToBottom();
 });
 
 $('#message-form').on('submit', function (e) {
